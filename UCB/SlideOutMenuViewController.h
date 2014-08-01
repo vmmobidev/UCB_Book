4e35b9c180e1e02bf80bad32e8fb1de67a998be8
ManoharaK <vmoksha@VMPC1174.local>
2014-08-01 17:08:04 +0530

    menu Listed Started

--- UCB/SlideOutMenuViewController.h
+++ UCB/SlideOutMenuViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SlideOutMenuViewController : UIViewController
+@interface SlideOutMenuViewController : UIViewController <UITableViewDataSource,UITabBarDelegate>
 
 @end
--- UCB/SlideOutMenuViewController.m
+++ UCB/SlideOutMenuViewController.m
@@ -9,7 +9,11 @@
 #import "SlideOutMenuViewController.h"
 
 @interface SlideOutMenuViewController ()
+{
+    NSArray *menuList, *listImagesArr;
+}
 
+@property (weak, nonatomic) IBOutlet UITableView *tableViewOutlet;
 @end
 
 @implementation SlideOutMenuViewController
@@ -27,6 +31,10 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    menuList = @[@"Directory",@"Card View",@"Holiday",@"Location",@"About Us",@"Logout"];
+    
+    listImagesArr = @[@"ic_directory.png",@"ic_cardView.png",@"ic_holidays.png",@"ic_location.png",@"ic_aboutUs@2x.png",@"ic_logout@2x.png"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,6 +42,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark
+#pragma mark UITableViewDataSource methods
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [menuList count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    UILabel *lable = (UILabel *)[cell viewWithTag:100];
+    lable.text = menuList[indexPath.row];
+    
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:200];
+    imageView.image = [UIImage imageNamed:listImagesArr[indexPath.row]];
+    
+    return cell;
+}
+
 
 /*
 #pragma mark - Navigation
